a1de6113af3c340c1a6e9eacbe86a1d7495214b6
Hoenn <skatevan1313@gmail.com>
2015-11-25 18:31:44 -0500

    Included arvydas's work around for crash on mode 3 morph

--- Endless/1min6ColorMorph.py
+++ Endless/1min6ColorMorph.py
@@ -4,13 +4,21 @@ from time import sleep
 for bstick in blinkstick.find_all():
     #clear lights
     bstick.turn_off()
+    numSteps=300
+    dur = 999
     try:
         while True:
-           bstick.morph(channel=0, steps=10, name="red", duration = 100)
-           bstick.morph(channel=0, steps=10, name="blue", duration = 100)
-           bstick.morph(channel=0, steps=10, name="green", duration = 100)
-           bstick.morph(channel=0, steps=10, name="yellow", duration = 100)
-           bstick.morph(channel=0, steps=10, name="purple", duration = 100)
-           bstick.morph(channel=0, steps=10, name="orange", duration = 100)
+            bstick.morph(channel=0, steps=numSteps, name="red", duration = dur)
+            sleep(.01)
+            bstick.morph(channel=0, steps=numSteps, name="blue", duration = dur)
+            sleep(.01)
+            bstick.morph(channel=0, steps=numSteps, name="green", duration = dur)
+            sleep(.01)
+            bstick.morph(channel=0, steps=numSteps, name="yellow", duration = dur)
+            sleep(.01)
+            bstick.morph(channel=0, steps=numSteps, name="cyan", duration = dur)
+            sleep(.01)
+            bstick.morph(channel=0, steps=numSteps, name="orange", duration = dur)
+            sleep(.01)
     except KeyboardInterrupt:
         pass
